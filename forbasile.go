b8d62377d177f23204c49bea5f523e271fdeb7b8
Niklas Rosencrantz <niklas@montao.com.br>
2019-01-14 03:31:42 +0100

    improve plugin generation

--- forbasile.go
+++ forbasile.go
@@ -7,10 +7,12 @@ import (
 	"path/filepath"
 	"plugin"
 	"strings"
+	"reflect"
+	"strconv"
 )
 
-type HI interface {
-	HI(int, int)
+type SUM interface {
+	SUMIT(x int, y int) int
 }
 
 func main() {
@@ -25,8 +27,8 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	strprg := fmt.Sprintf("package main\nimport \"fmt\"\ntype %s string\nfunc(s %s) %s(a int, b int){ fmt.Println(\"%s\")}\nvar %s %s", strings.ToLower(os.Args[1]), strings.ToLower(os.Args[1]), strings.Title(os.Args[1]), os.Args[1], strings.Title(os.Args[1]), strings.ToLower(os.Args[1]))
-	//program := "package main\nimport \"fmt\"\ntype sumsquare string\nfunc (s sumsquare) Sumsquare(a int, b int) { fmt.Println(\"HI\")}\nvar Sumsquare sumsquare"
+	strprg := fmt.Sprintf("package main\ntype %s string\nfunc(s %s) SUMIT (x int, y int) int { %s}\nvar %s %s", strings.ToLower(os.Args[1]), strings.ToLower(os.Args[1]), os.Args[2], strings.Title(os.Args[1]), strings.ToLower(os.Args[1]))
+
 	l, err := f.WriteString(strprg)
 	if err != nil {
 		fmt.Println(err)
@@ -47,8 +49,8 @@ func main() {
 	exPath := filepath.Dir(ex)
 	fmt.Println(exPath)
 
-	cmd := exec.Command("go", "build", "-buildmode=plugin", "-o", "/home/developer/proj/gitlab.com/forbasile/HI/HI.so", "/home/developer/proj/gitlab.com/forbasile/HI/HI.go")
-	//cmd.Dir = "/home/developer/proj/gitlab.com/forbasile"
+	cmd := exec.Command("go", "build", "-buildmode=plugin", "-o", "/home/developer/proj/gitlab.com/forbasile/SUM/SUM.so", "/home/developer/proj/gitlab.com/forbasile/SUM/SUM.go")
+
 	out, err2 := cmd.Output()
 	fmt.Println(out)
 
@@ -66,23 +68,26 @@ func main() {
 	}
 
 	// 2. look up a symbol (an exported function or variable)
-	// in this case, variable Sum
-	symHI, err := plug.Lookup("HI")
+	// in this case, variable os.Args[1]
+	symX, err := plug.Lookup(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
 	// 3. Assert that loaded symbol is of a desired type
-	// in this case interface type Sum (defined above)
-	var hi HI
-	hi, ok := symHI.(HI)
+	// in this case interface type X (defined above)
+	var sum SUM
+	sum, ok := symX.(SUM)
 	if !ok {
-		fmt.Println("unexpected type from module symbol")
+		fmt.Println(fmt.Sprintf("unexpected type from module symbol %s", reflect.TypeOf(symX.(SUM))))
 		os.Exit(1)
 	}
 
 	// 4. use the module
-	hi.HI(5, 3)
+	x1, err := strconv.Atoi(os.Args[3])
+	y1, err := strconv.Atoi(os.Args[4])
+
+	fmt.Println(sum.SUMIT(x1, y1))
 
 }
